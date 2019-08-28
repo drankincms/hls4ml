06cdbb0b8765baae859aec2da0d248e48d14ea27
Dylan R <dylansr90@gmail.com>
2019-08-28 14:11:05 -0500

    adding std=c++0x flag

--- hls-template/generate_gal_send.tcl
+++ hls-template/generate_gal_send.tcl
@@ -3,7 +3,7 @@ set_top kern_send
 open_solution "solution1"
 set_part {xc7vx690tffg1927-2}
 add_files firmware/myproject.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x"
-add_files firmware/galapagos_kerns.cpp -cflags "-I firmware -I firmware/weights -I[file normalize nnet_utils]"
+add_files firmware/galapagos_kerns.cpp -cflags "-I firmware -I firmware/weights -I[file normalize nnet_utils] -std=c++0x"
 catch {config_array_partition -maximum_size 4096}
 create_clock -period 5 -name default
 config_interface -expose_global
