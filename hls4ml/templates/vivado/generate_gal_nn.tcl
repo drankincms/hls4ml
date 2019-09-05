7e99d8906b09310bb92989ea97832c34d8736459
Dylan Rankin <dylansr90@gmail.com>
2019-09-05 13:50:08 +0000

    changing path labels

--- hls4ml/templates/vivado/generate_gal_nn.tcl
+++ hls4ml/templates/vivado/generate_gal_nn.tcl
@@ -8,9 +8,9 @@ cd $galapagos_path/hlsBuild/${board_name}/ip
 open_project kern_nn
 set_top kern_nn
 open_solution "solution1"
-set_part {xc7vx690tffg1927-2}
-add_files firmware/myproject.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x"
-add_files firmware/galapagos_kerns.cpp -cflags "-I $galapagos_path/middleware/include -I firmware -I firmware/weights -I[file normalize nnet_utils] -std=c++0x"
+set_part ${part_name}
+add_files $src_path_root/firmware/myproject.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x"
+add_files $src_path_root/firmware/galapagos_kerns.cpp -cflags "-I $galapagos_path/middleware/include -I $src_path_root/firmware -I $src_path_root/firmware/weights -I[file normalize nnet_utils] -std=c++0x"
 catch {config_array_partition -maximum_size 4096}
 create_clock -period 5 -name default
 config_interface -expose_global
